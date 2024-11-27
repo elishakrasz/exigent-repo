1467748d78ceef2b9913ec78da8a7b04e8367f54
elishakrasz <elishashestov@gmail.com>
2024-11-27 08:46:49 +0200

    eslint file

---
+++ nextjs-exigent-upgrade/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // Disable the rule globally
+    },
+  };
+  
